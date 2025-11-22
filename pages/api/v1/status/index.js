cb784aa5e97aaf7ec214cf3f7caba6218d84476c
Paulo Feresin <paulo.feresin@gmail.com>
2025-11-22 20:16:00 +0000

    add database connections info and tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: result });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseConnections = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const databaseConnectionsValue = databaseConnections.rows[0].count;
+
+  const maxConnections = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnections.rows[0].max_connections;
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        current_connections: databaseConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
